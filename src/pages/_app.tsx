5bd16fb8ea388de1f0ec8e51dce20a10b0f612f0
Mario Paglia <mpagliajr@gmail.com>
2023-01-29 02:09:30 -0300

    Fix initial project build

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
 
-import GlobalStyle from './styles/global';
-import theme from './styles/theme';
+import GlobalStyle from '../styles/global';
+import theme from '../styles/theme';
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
 	return (
